50f3e0124755f039f7c580e3fae83651ca665fcb
jenny7hi <33742243+jenny7hi@users.noreply.github.com>
2018-05-05 21:45:56 -0700

    search and profile pages

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AppService {
-
+  public preLoginMenuState
   constructor() {
     this.preLoginMenuState = "closed";
   }
